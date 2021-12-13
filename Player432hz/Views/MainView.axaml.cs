e926cfaa744574b692110abdadf07a5b20a623ba
Etienne Charland <mysteryx93@protonmail.com>
2021-12-13 13:38:50 -0500

    Moved icons into Assets

---
+++ Player432hz/Views/MainView.axaml.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HanumanInstitute.Player432hz.Views;
+
+/// <summary>
+/// Interaction logic for MainView.xaml
+/// </summary>
+public partial class MainView : Window
+{
+    public MainView()
+    {
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
