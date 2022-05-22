241d2435a8043d30a76abc6d0367af6dd3bc2a62
Etienne Charland <mysteryx93@protonmail.com>
2022-05-22 00:06:33 -0500

    Fixed audio file extensions list

--- Player432hz/Business/AppPathService.cs
+++ Player432hz/Business/AppPathService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using HanumanInstitute.Common.Services;
+using HanumanInstitute.MediaPlayer.Avalonia.Bass;
 
 namespace HanumanInstitute.Player432hz.Business;
 
@@ -18,21 +20,21 @@ public class AppPathService : IAppPathService
         _fileSystem = fileSystemService ?? throw new ArgumentNullException(nameof(fileSystemService));
     }
 
-    /// <summary>
-    /// Returns all valid video extensions.
-    /// </summary>
-    public IList<string> VideoExtensions => _videoExtensions ??= new List<string> { ".mp4", ".webm", ".avi", ".flv", ".mpg", ".mkv", ".wmv", ".tp", ".ts", ".mov", ".avs", ".m2v", ".vob" };
-    private IList<string>? _videoExtensions;
+    // /// <summary>
+    // /// Returns all valid video extensions.
+    // /// </summary>
+    // public IList<string> VideoExtensions => _videoExtensions ??= new List<string> { ".mp4", ".webm", ".avi", ".flv", ".mpg", ".mkv", ".wmv", ".tp", ".ts", ".mov", ".avs", ".m2v", ".vob" };
+    // private IList<string>? _videoExtensions;
     /// <summary>
     /// Returns all valid audio extensions
     /// </summary>
-    public IList<string> AudioExtensions => _audioExtensions ??= new List<string> { ".mp3", ".mp2", ".aac", ".wav", ".wma", ".m4a", ".flac" };
+    public IList<string> AudioExtensions => _audioExtensions ??= BassDevice.Instance.SupportedExtensions.Select(x => x.Extensions).SelectMany(x => x).ToList();
     private IList<string>? _audioExtensions;
-    /// <summary>
-    /// Returns all valid image extensions.
-    /// </summary>
-    public IList<string> ImageExtensions => _imageExtensions ??= new List<string> { ".gif", ".jpg", ".png", ".bmp", ".tiff" };
-    private IList<string>? _imageExtensions;
+    // /// <summary>
+    // /// Returns all valid image extensions.
+    // /// </summary>
+    // public IList<string> ImageExtensions => _imageExtensions ??= new List<string> { ".gif", ".jpg", ".png", ".bmp", ".tiff" };
+    // private IList<string>? _imageExtensions;
 
     /// <summary>
     /// Returns the path where unhandled exceptions are logged.
--- Player432hz/Business/IAppPathService.cs
+++ Player432hz/Business/IAppPathService.cs
@@ -6,17 +6,9 @@ namespace HanumanInstitute.Player432hz.Business;
 public interface IAppPathService
 {
     /// <summary>
-    /// Returns all valid video extensions.
-    /// </summary>
-    IList<string> VideoExtensions { get; }
-    /// <summary>
     /// Returns all valid audio extensions
     /// </summary>
     IList<string> AudioExtensions { get; }
-    /// <summary>
-    /// Returns all valid image extensions.
-    /// </summary>
-    IList<string> ImageExtensions { get; }
     /// <summary>
     /// Returns the path where unhandled exceptions are logged.
     /// </summary>
