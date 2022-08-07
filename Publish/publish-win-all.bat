a3aa9fe849d33ad6361562424471c07944882b0e
Etienne Charland <mysteryx93@protonmail.com>
2022-08-07 03:29:28 -0500

    Add YangDownloader to publish

--- Publish/publish-win-all.bat
+++ Publish/publish-win-all.bat
@@ -7,3 +7,6 @@ call publish-win Converter432hz win-x86
 
 call publish-win PowerliminalsPlayer win-x64
 call publish-win PowerliminalsPlayer win-x86
+
+call publish-win YangDownloader win-x64
+call publish-win YangDownloader win-x86
